301ded8e19bad948b5133a145802b14c8f68c628
Lxler <10110553+lxlstudyprogram@user.noreply.gitee.com>
2024-04-26 17:05:09 +0800

    init

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ALL(v) (v).begin(), (v).end()
+#define pb push_back
+#define V vector
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using LL = long long;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("in.in", "r", stdin);
+	freopen("out.out", "w", stdout);
+	
+
+
+	return 0;
+}
